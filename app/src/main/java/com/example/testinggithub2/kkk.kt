512157bbb1c477610c445a71c36b1f62df651473
Rong0411 <57433757+Rong0411@users.noreply.github.com>
2019-12-17 22:25:12 +0800

    Revert "This"

--- app/src/main/java/com/example/testinggithub2/kkk.kt
+++ app/src/main/java/com/example/testinggithub2/kkk.kt
@@ -3,7 +3,7 @@ package com.example.testinggithub2
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class kkk : AppCompatActivity() {
+class kkk : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_xxx)
@@ -12,8 +12,8 @@ class kkk : AppCompatActivity() {
         // from
         //Shu Rong
         //Bye bye
-        //lets gooo
-        //hahaaha
-        //oh gosh
+        //newly update
+        //blablabla
+        //omg
     }
 }
