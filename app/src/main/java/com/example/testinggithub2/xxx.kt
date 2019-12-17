6ffdc4136264391789c8ec9756584d9b04f7c4ea
Rong0411 <57433757+Rong0411@users.noreply.github.com>
2019-12-17 14:35:01 +0800

    Init

---
+++ app/src/main/java/com/example/testinggithub2/xxx.kt
@@ -0,0 +1,14 @@
+package com.example.testinggithub2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class xxx : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_xxx)
+
+        // Shu Rong here
+    }
+}
