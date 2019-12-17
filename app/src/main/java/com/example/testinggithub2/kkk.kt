06d899893858029c8d6a6d2e4a962fa35c144823
Rong0411 <57433757+Rong0411@users.noreply.github.com>
2019-12-17 21:26:44 +0800

    init

---
+++ app/src/main/java/com/example/testinggithub2/kkk.kt
@@ -0,0 +1,17 @@
+package com.example.testinggithub2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class kkk : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_xxx)
+
+        // Halooooooo
+        // from
+        //Shu Rong
+        //Bye bye
+        //newly update
+    }
+}
